415ff6a43737226bd7b245e62f38afaab63a648c
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-13 20:35:12 -0700

    complete section 16

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function photos(){
+        return $this->morphMany('App\Models\Photo', 'imageable');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Staff;
+use App\Models\Photo;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function (){
+    $staff = Staff::find(1);
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function (){
+    $staff = Staff::findOrFail(1);
+    foreach ($staff->photos as $photo) {
+        return $photo->path;
+    }
+});
+
+Route::get('/update', function (){
+    $staff = Staff::findOrFail(1);
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path = "Updated";
+    $photo->save();
+});
+
+Route::get('/delete', function (){
+    $staff = Staff::find(1);
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function (){
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(2);
+    $staff->photos()->save($photo);
 });
